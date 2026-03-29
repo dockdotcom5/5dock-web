bd3991c36b758484ed6e5e6fa5ec39fb42693082
ylytdeng <ylytdeng@users.noreply.github.com>
2026-03-29 14:48:07 +0800

    Updates

--- _next/static/mELDwGvHxT0AabGYooX_s/_buildManifest.js
+++ _next/static/mELDwGvHxT0AabGYooX_s/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/5dock-web//_next/:path+",
+        "destination": "/5dock-web/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
